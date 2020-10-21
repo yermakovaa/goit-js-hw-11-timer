0b6ff1e0cdf2bb8041b15a7476ea9ce0041ec091
Anastasiia Yermakova <yermak1037@gmail.com>
2020-10-21 14:06:32 +0300

    Create js

---
+++ src/js/timer.js
@@ -0,0 +1,51 @@
+const refs = {
+  days: document.querySelector('span[data-value="days"]'),
+  hours: document.querySelector('span[data-value="hours"]'),
+  mins: document.querySelector('span[data-value="mins"]'),
+  secs: document.querySelector('span[data-value="secs"]'),
+};
+
+class CountdownTimer {
+  constructor({ targetDate }) {
+    this.targetDate = targetDate;
+    this.init();
+  }
+
+  init() {
+    setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = this.targetDate - currentTime;
+      const time = this.getTimeComponents(deltaTime);
+
+      this.updateClockface(time);
+    }, 1000);
+  }
+
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+
+  // Метод для рассчета дней, часов, минут и секунд:
+  getTimeComponents(time) {
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+  }
+
+  updateClockface({ days, hours, mins, secs }) {
+    refs.days.textContent = `${days}`;
+    refs.hours.textContent = `${hours}`;
+    refs.mins.textContent = `${mins}`;
+    refs.secs.textContent = `${secs}`;
+  }
+}
+
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('2021, 2, 14'),
+});
